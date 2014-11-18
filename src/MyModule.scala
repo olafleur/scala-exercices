cb130b3b4facaccbb6b6cb8a50d9daef7706418b
Olivier Lafleur <olivier.lafleur@gmail.com>
2014-11-18 11:29:02 -0500

    isSorted

--- src/MyModule.scala
+++ src/MyModule.scala
@@ -22,11 +22,22 @@ object MyModule {
   }
 
   def fib(n: Int): Int = {
+    @annotation.tailrec
     def loop(acc: Int, m: Int, i: Int): Int = {
       if (i == n) acc
       else loop(m, m + acc, i + 1)
     }
     loop(0, 1, 1)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Boolean = {
+      if (n >= as.length) true
+      else if (!ordered(as(n - 1), as(n))) false
+      else loop(n + 1)
+    }
+    loop(1)
   }
 
   def main(args: Array[String]): Unit = {
@@ -35,5 +46,11 @@ object MyModule {
     println(factorial(5).toString)
 
     println(fib(5))
+
+    if (isSorted[Int](Array(1, 2, 3), (x, y) => x <= y)) {
+      println("Trié")
+    } else {
+      println("Non trié")
+    }
   }
 }
